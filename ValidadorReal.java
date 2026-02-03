5169e696ed511368d1cdb2cf25bf9aa9770e9dff
Klaywer2019 <keisejemima@gmail.com>
2026-02-03 14:03:18 -0300

    Criar ValidadorReal.java

---
+++ ValidadorReal.java
@@ -0,0 +1,6 @@
+public class ValidadorReal {
+    public boolean validarAcesso(String email) {
+        // Lógica ultra secreta pra validar o criador
+        return email.contains("@") && email.length() > 5;
+    }
+}
